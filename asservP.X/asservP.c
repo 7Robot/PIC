c565eee6e04beba78575791433712413f0668d55
Maxime Morin <maxime.morin7@gmail.com>
2012-01-26 20:48:32 +0100

    On a un PI et des profils !

--- asservP.X/asservP.c
+++ asservP.X/asservP.c
@@ -5,6 +5,7 @@
 #include <portb.h>
 #include <timers.h>
 #include <pwm.h>
+#include <math.h>
 
 /////*CONFIGURATION*/////
 #pragma config RETEN = OFF
@@ -30,6 +31,9 @@ void GsetDC(int dc);
 void DsetDC(int dc);
 void high_isr(void);
 void low_isr(void);
+void DelayMS(int delay);
+void Vconsigne(int Vg, int Vd);
+void resetTicks(void);
 
 /////*CONSTANTES*/////
 #define led LATAbits.LATA5
@@ -44,13 +48,30 @@ void low_isr(void);
 #define DchA PORTBbits.RB1
 #define DchB PORTBbits.RB5
 
+#define riseGchA INTCON2bits.INTEDG0
+#define riseDchA INTCON2bits.INTEDG1
+
+
+#define TourRoue 3072 // ticks par tour de roue
+#define Vmax 80
+#define TicksAcc 3*TourRoue
+
+
+
 #define Kp 10
+#define Ki 10
+
+#define Kr 1 // Conversion ticks-mm
+ //Pente acc/decceleration
+
 /////*VARIABLES GLOBALES*/////
 unsigned int i=0;
-long gTicks=0, dTicks=0;
+long gTicks=0, dTicks=0, r=0;
+long Rconsigne=0, Eposition=0;
 long dTicksp=0, gTicksp=0;
 int gVitesse=0, dVitesse=0;
-int cg = 0, cd=0;
+int gConsigne = 0, dConsigne=0, dErreur=0, gErreur=0, gIErreur=0, dIErreur=0;
+char Gsens = 1, Dsens = 1, prevB = 0,position=0;
 
 
 /////*Interruptions*/////
@@ -69,65 +90,83 @@ void low_interrupt(void)
 #pragma interrupt high_isr
 void high_isr(void)
 {
+   
+   /* Interruptions GchA, detecte fronts, determine le sens, et in(dé)cremente*/
+   if(INTCONbits.INT0IE && INTCONbits.INT0IF)
+   {
+       INTCONbits.INT0IF = 0; // flag au debut pour capter autre
+       Gsens = -1;
+       if((riseGchA && GchB) || (!riseGchA && !GchB)) //front montant
+          Gsens = 1;
 
-    //Encodeur gauche
-    if(INTCONbits.INT0IE && INTCONbits.INT0IF) //GchA interrupt
-    {
-        if(GchB) gTicks ++;
-        else gTicks--;
+       riseGchA = riseGchA^1;
+       gTicks += Gsens;
+   }
 
-        INTCONbits.INT0IF = 0;
-    }
+   /* Interruptions DchA, deteche fronts, determine sens, et in(dé)crémente*/
+   if(INTCON3bits.INT1E && INTCON3bits.INT1IF)
+   {
+       INTCON3bits.INT1IF = 0;
+       Dsens = 1;
+       if((riseDchA && DchB) || (!riseDchA && !DchB)) //front montant
+          Dsens = -1;
 
-   /* if(INTCONbits.RBIE && INTCONbits.RBIF && GchB) //GchB interrupt
-    {
-        INTCONbits.RBIF = 0; // au debut au cas ou DchB redeclenche
+       riseDchA = riseDchA^1;
+       dTicks += Dsens;
+   }
 
-        if(!GchA) gTicks ++;
-        else gTicks--;
-    }*/
+   /*Interruptions GchB et DchB, détecte fronts, utilise le sens déterminé par les autres*/
+   if(INTCONbits.RBIE && INTCONbits.RBIF)
+   {
+       if((PORTB^prevB) & 0b00010000) gTicks += Gsens;
+       else if((PORTB^prevB) & 0b00100000) dTicks += Dsens;
+       prevB = PORTB; //très important
+       INTCONbits.RBIF = 0; // mettre à la fin car modifier par PORTB
+   }
 
+    
+}
 
-    //Encodeur droit
-    if(INTCON3bits.INT1IE && INTCON3bits.INT1IF) //DchA interrupt
-    {
-        if(!DchB) dTicks ++;
-        else dTicks--;
-        INTCON3bits.INT1IF = 0;
-    }
-
-   /* if(INTCONbits.RBIE && INTCONbits.RBIF && DchB) //DchB interrupt
-    {
-        INTCONbits.RBIF = 0; // au debut au cas ou GchB redeclenche
-       if(DchA) dTicks ++;
-        else dTicks--;
-    }*/
-
-    //Calcul asserv
+#pragma interrupt low_isr
+void low_isr(void)
+{
+    
     if(INTCONbits.TMR0IE && INTCONbits.TMR0IF)
     {
-
+        /* Calcul vitesse*/
         gVitesse = gTicks - gTicksp;
         dVitesse = dTicks - dTicksp;
-
         gTicksp = gTicks;
         dTicksp = dTicks;
 
-        GsetDC(Kp*(cg-gVitesse));
-        DsetDC(Kp*(cg-dVitesse));
+        /* Calcul erreur */
+        dErreur = dConsigne - dVitesse;
+        gErreur = gConsigne - gVitesse;
+
+        dIErreur += dErreur;
+        gIErreur += gErreur;
+
+        /* Commande des moteurs */
+        GsetDC(Kp*gErreur + Ki*gIErreur);
+        DsetDC(Kp*dErreur + Ki*dIErreur);
+
+        
+        /* Calcul profil. */
+        if(position)
+        {
+            r = Kr*(gTicks+dTicks)/2;
+            Eposition = (Rconsigne-fabs(2*r-Rconsigne))/2;
+            Eposition = Eposition/20 ;
+            if(Eposition > Vmax) Eposition = Vmax;
+            Vconsigne(Eposition+1,Eposition+1);
+        }
+
         led = led^1;
         INTCONbits.TMR0IF = 0; //On efface tous les flags pour faire comme
         INTCONbits.INT0IF = 0; // si rien ne c'etait passé
         INTCON3bits.INT1IF = 0;
         INTCONbits.RBIF = 0;
     }
-
-}
-
-#pragma interrupt low_isr
-void low_isr(void)
-{
-
 }
 
 
@@ -157,25 +196,41 @@ void main (void)
     /*Interruptions portB*/
     OpenRB0INT( PORTB_CHANGE_INT_ON & RISING_EDGE_INT & PORTB_PULLUPS_OFF);
     OpenRB1INT( PORTB_CHANGE_INT_ON & RISING_EDGE_INT & PORTB_PULLUPS_OFF);
-    OpenPORTB( PORTB_CHANGE_INT_ON & PORTB_PULLUPS_ON);
-    IOCB = 0b00110000;
+    OpenPORTB( PORTB_CHANGE_INT_ON & PORTB_PULLUPS_OFF);
+    IOCB = 0b00110000 ;
 
     /*Timer0 interrupt pour calcul asserv*/
-    OpenTimer0( TIMER_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_8 );
+    OpenTimer0( TIMER_INT_ON & T0_16BIT & T0_SOURCE_INT & T0_PS_1_4 );
+    INTCON2bits.TMR0IP = 0 ; //Basse priorité pour ne pas rater de ticks 
+    /* Signal de démarrage du programme. */
+    led = 0;
+    for(i=0;i<20;i++)
+    {
+        led=led^1;
+        DelayMS(50);
+    }
+
+    RCONbits.IPEN = 1; // priorités, par défaut : haute
+    INTCONbits.GIEH = 1;
+    INTCONbits.GIEL = 1;
+
+    resetTicks();
+    position = 1;
+    Rconsigne = 1*TourRoue;
 
 
-    INTCONbits.GIE = 1;
-
-
-   
-       
-        cg = 25 ;
-        cd = 25 ;
-        for(i=0;i<=10;i++) Delay10KTCYx(255);
-        cd=0;
-        cg=0;
         while(1);
   
+}
+
+
+void DelayMS(int delay)
+{
+    int cp = 0;
+    for(cp=0; cp<delay; cp++)
+    {
+        Delay1KTCYx(16);
+    }
 }
 
 void GsetDC(int dc)
@@ -215,4 +270,19 @@ void DsetDC(int dc)
         dc = 1023;
 
     SetDCPWM3(dc);
+}
+
+void Vconsigne(int Vg, int Vd)
+{
+    /* Consigne de vitesse pour les moteurs entre 0 et Vmax. */
+    gConsigne = Vg;
+    dConsigne = Vd;
+    if(Vg >= Vmax) gConsigne= Vmax;
+    if(Vd >= Vmax) dConsigne= Vmax;
+}
+
+void resetTicks(void)
+{
+    gTicks = 0;
+    dTicks = 0;
 }
