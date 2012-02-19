40689aafe5aef2d4b23f355371a4bb1425c92d91
Maxime Morin <maxime.morin7@gmail.com>
2012-02-19 17:32:17 +0100

    Romain, le template de la tourelle (AlimP.X) est prêt...

--- alimP.X/alimP.c
+++ alimP.X/alimP.c
@@ -1,5 +1,5 @@
 /*
-* Programme ARM petit robot
+* Programme carte alim petit robot
 * Eurobot 2012
 * Compiler : Microchip C18
 * µC : 18f2680
@@ -18,6 +18,9 @@
 #include <timers.h>
 #include <p18f2680.h>
 #include "../libcan/can18xx8.h"
+#include <usart.h>
+#include <delays.h>
+#include <portb.h>
 
 /////*CONFIGURATION*/////
 #pragma config OSC = HS
@@ -34,21 +37,21 @@
 #pragma config LVP = OFF
 
 /////*CONSTANTES*/////
-#define XTAL    20000000
-#define led     PORTCbits.RC0
+#define XTAL        20000000
+#define led         PORTCbits.RC0
 
 
 /////*PROTOTYPES*/////
 void high_isr(void);
 void low_isr(void);
-void DelayMS(int delay);
+
 
 /////*VARIABLES GLOBALES*/////
-int i=0;
+int i = 0;
 CANmsg message;
 
-/////*INTERRUPTIONS*/////
 
+/////*INTERRUPTIONS*/////
 #pragma code high_vector=0x08
 void high_interrupt(void)
 {
@@ -70,108 +73,86 @@ void high_isr(void)
 #pragma interrupt low_isr
 void low_isr(void)
 {
+    // Réception CAN.
     if(PIE3bits.RXB0IE && PIR3bits.RXB0IF)
     {
 
-        /*On stocke la valeur. */
-        while(CANIsRxReady())
-        {
-            CANReceiveMessage(&message.id,message.data,
-                          &message.len,&message.flags);
+        while(CANIsRxReady()) {
+            CANReceiveMessage(&message.id, message.data, &message.len, &message.flags);
         }
-        led = led^1;
+        
+        switch (message.id) {
+                    case 132: //Renvoyer distace/angle objet le plus proche
+                      //CODE
+                      break;
+                    case 133: //Renvoyer toutes les infos
+                      //CODE
+                      break;
+                    case 134: //Broadcast ON
+                      //CODE
+                      break;
+                    case 135: //Boradcast OFF
+                      //CODE
+                      break;
+                      
+                    default:
+                      // Rien
+                      break;
+                    }
 
+        led = led ^1;
 
-        PIR3bits.RXB0IF=0;
-        PIR3bits.RXB1IF=0;
-        PIR3bits.ERRIF=0;
-
+        PIR3bits.RXB0IF = 0;
     }
-
-    if(PIE3bits.ERRIE && PIR3bits.ERRIF)
-    {
-        /*On stocke la valeur. */
-        while(CANIsRxReady())
-        {
-            CANReceiveMessage(&message.id,message.data,
-                          &message.len,&message.flags);
-        }
-
-        led = led^1;
-
-
-        //On stocke le messgae mais on n'incremente pas le buffer
-        PIR3bits.RXB0IF=0;
-        PIR3bits.RXB1IF=0;
-        PIR3bits.ERRIF=0;
-    }
-
 
 }
 
-
 /////*PROGRAMME PRINCIPAL*/////
-void main (void)
-{
-    /* Initialisations. */
-    ADCON1 = 0x0F ;
+void main (void) {
+    // Initialisations.
+    ADCON1 = 0x0F;
     ADCON0 = 0b00000000;
-    WDTCON = 0 ;
+    WDTCON = 0;
 
-    /* Configurations. */
-    TRISA   = 0xFF ;
-    TRISB   = 0xFF ;
-    TRISC   = 0b11111110 ;
+    // Configurations.
+    TRISA  = 0b11111111;
+    TRISB  = 0b11111111;
+    TRISC  = 0b11111110;
 
-    // Interruptions Buffer1
-/*    IPR3bits.RXB1IP=1;// : priorité haute par defaut du buff 1
-    PIE3bits.RXB1IE=1;//autorise int sur buff1
-    PIR3bits.RXB1IF=0;//mise a 0 du flag*/
-
-    // Interruption Buffer 0
-    IPR3bits.RXB0IP=0;// : priorité basse par defaut du buff 0
-    PIE3bits.RXB0IE=1;//autorise int sur buff0
-    PIR3bits.RXB0IF=0;//mise a 0 du flag
-    PIR3bits.ERRIF=0; //flag erreur a 0
-    PIE3bits.ERRIE=1; //autorise int erreur
-
-       // Configuration des masques et filtres
-    // Set CAN module into configuration mode
+    
+    // Configuration du CAN.
+    CANInitialize(1, 5, 7, 6, 2, CAN_CONFIG_VALID_STD_MSG);
+    // Configuration des masques et filtres.
     CANSetOperationMode(CAN_OP_MODE_CONFIG);
-    // Set Buffer 1 Mask value
-    CANSetMask(CAN_MASK_B1, 0b00010000000,CAN_CONFIG_STD_MSG);
-    // Set Buffer 2 Mask value
-    CANSetMask(CAN_MASK_B2, 0xFFFFFF ,CAN_CONFIG_STD_MSG );
-    // Set Buffer 1 Filter values
-    CANSetFilter(CAN_FILTER_B1_F1,0b00010000000,CAN_CONFIG_STD_MSG );
-    CANSetFilter(CAN_FILTER_B1_F2,0b0000,CAN_CONFIG_STD_MSG );
-    CANSetFilter(CAN_FILTER_B2_F1,0b0000,CAN_CONFIG_STD_MSG );
-    CANSetFilter(CAN_FILTER_B2_F2,0b0000,CAN_CONFIG_STD_MSG );
-    CANSetFilter(CAN_FILTER_B2_F3,0b0000,CAN_CONFIG_STD_MSG );
-    CANSetFilter(CAN_FILTER_B2_F4,0b0000,CAN_CONFIG_STD_MSG );
-    // Set CAN module into Normal mode
+    // Set Buffer 1 Mask value.
+    CANSetMask(CAN_MASK_B1, 0b00010000000, CAN_CONFIG_STD_MSG);
+    // Set Buffer 1 Filter values.
+    CANSetFilter(CAN_FILTER_B1_F1, 0b00010000000, CAN_CONFIG_STD_MSG);
+    CANSetFilter(CAN_FILTER_B1_F2, 0b00010000000, CAN_CONFIG_STD_MSG);
+    // Set CAN module into Normal mode.
     CANSetOperationMode(CAN_OP_MODE_NORMAL);
+    // Interruption Buffer 0.
+    IPR3bits.RXB0IP = 0; // Priorité basse.
+    PIE3bits.RXB0IE = 1; // Activée.
+    PIR3bits.RXB0IF = 0;
+    // Interruptions Buffer 1.
+    PIE3bits.RXB1IE = 0; // Interdite.
 
 
-    /* Signal de démarrage du programme. */
+    // Signal de démarrage du programme.
     led = 0;
-    for(i=0;i<20;i++)
-    {
-        led=led^1;
+    for(i = 0; i < 20; i++) {
+        led = led ^ 1;
         DelayMS(50);
     }
 
-    INTCONbits.GIE = 0; /* Autorise interruptions. */
-    led = 0;
+    //Autorisation des interruptions
+    RCONbits.IPEN = 1;
+    INTCONbits.GIEH = 1; 
+    INTCONbits.GIEL = 1;
 
 
-    /* Boucle principale. */
-     while(1)
-    {
+    while(1) {
 
     }
 }
-
-///// Définition des fonctions du programme. /////
-
-
