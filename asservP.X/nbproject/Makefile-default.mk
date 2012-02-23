c614025f6864647fd62648755b5525db65e395cd
Martin d'Allens <caerbazar@gmail.com>
2012-02-23 16:13:54 +0100

    Dégage dossiers inutiles.

--- asservP.X/nbproject/Makefile-default.mk
+++ asservP.X/nbproject/Makefile-default.mk
@@ -39,11 +39,11 @@ OBJECTDIR=build/${CND_CONF}/${IMAGE_TYPE}
 DISTDIR=dist/${CND_CONF}/${IMAGE_TYPE}
 
 # Object Files Quoted if spaced
-OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1846633221/asservP.o ${OBJECTDIR}/_ext/1018694054/can18xxk8.o
-POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1846633221/asservP.o.d ${OBJECTDIR}/_ext/1018694054/can18xxk8.o.d
+OBJECTFILES_QUOTED_IF_SPACED=${OBJECTDIR}/_ext/1018694054/can18xxk8.o ${OBJECTDIR}/asservP.o
+POSSIBLE_DEPFILES=${OBJECTDIR}/_ext/1018694054/can18xxk8.o.d ${OBJECTDIR}/asservP.o.d
 
 # Object Files
-OBJECTFILES=${OBJECTDIR}/_ext/1846633221/asservP.o ${OBJECTDIR}/_ext/1018694054/can18xxk8.o
+OBJECTFILES=${OBJECTDIR}/_ext/1018694054/can18xxk8.o ${OBJECTDIR}/asservP.o
 
 
 CFLAGS=
@@ -89,30 +89,30 @@ endif
 # ------------------------------------------------------------------------------------
 # Rules for buildStep: compile
 ifeq ($(TYPE_IMAGE), DEBUG_RUN)
-${OBJECTDIR}/_ext/1846633221/asservP.o: /media/Données/Documents/7robot/PIC/asservP.X/asservP.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} ${OBJECTDIR}/_ext/1846633221 
-	@${RM} ${OBJECTDIR}/_ext/1846633221/asservP.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -p$(MP_PROCESSOR_OPTION)   -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1846633221/asservP.o   /media/Données/Documents/7robot/PIC/asservP.X/asservP.c 
-	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1846633221/asservP.o 
-	
 ${OBJECTDIR}/_ext/1018694054/can18xxk8.o: ../libcan/can18xxk8.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1018694054 
 	@${RM} ${OBJECTDIR}/_ext/1018694054/can18xxk8.o.d 
 	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -p$(MP_PROCESSOR_OPTION)   -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1018694054/can18xxk8.o   ../libcan/can18xxk8.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1018694054/can18xxk8.o 
 	
-else
-${OBJECTDIR}/_ext/1846633221/asservP.o: /media/Données/Documents/7robot/PIC/asservP.X/asservP.c  nbproject/Makefile-${CND_CONF}.mk
-	@${MKDIR} ${OBJECTDIR}/_ext/1846633221 
-	@${RM} ${OBJECTDIR}/_ext/1846633221/asservP.o.d 
-	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION)   -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1846633221/asservP.o   /media/Données/Documents/7robot/PIC/asservP.X/asservP.c 
-	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1846633221/asservP.o 
+${OBJECTDIR}/asservP.o: asservP.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR} 
+	@${RM} ${OBJECTDIR}/asservP.o.d 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -D__DEBUG -D__MPLAB_DEBUGGER_PK3=1 -p$(MP_PROCESSOR_OPTION)   -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/asservP.o   asservP.c 
+	@${DEP_GEN} -d ${OBJECTDIR}/asservP.o 
 	
+else
 ${OBJECTDIR}/_ext/1018694054/can18xxk8.o: ../libcan/can18xxk8.c  nbproject/Makefile-${CND_CONF}.mk
 	@${MKDIR} ${OBJECTDIR}/_ext/1018694054 
 	@${RM} ${OBJECTDIR}/_ext/1018694054/can18xxk8.o.d 
 	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION)   -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/_ext/1018694054/can18xxk8.o   ../libcan/can18xxk8.c 
 	@${DEP_GEN} -d ${OBJECTDIR}/_ext/1018694054/can18xxk8.o 
+	
+${OBJECTDIR}/asservP.o: asservP.c  nbproject/Makefile-${CND_CONF}.mk
+	@${MKDIR} ${OBJECTDIR} 
+	@${RM} ${OBJECTDIR}/asservP.o.d 
+	${MP_CC} $(MP_EXTRA_CC_PRE) -p$(MP_PROCESSOR_OPTION)   -I ${MP_CC_DIR}/../h  -fo ${OBJECTDIR}/asservP.o   asservP.c 
+	@${DEP_GEN} -d ${OBJECTDIR}/asservP.o 
 	
 endif
 
