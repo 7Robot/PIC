08d9d1ccc3ff0d5210494c6e150b5c6a4b009883
Martin d'Allens <caerbazar@gmail.com>
2012-02-29 00:33:08 +0100

    ok pour le test de reset par CAN

--- armP.X/armP.c
+++ armP.X/armP.c
@@ -1,17 +1,17 @@
 /*
-* Programme PIC ARM petit robot
-* Eurobot 2012
-* Compiler : Microchip C18
-* µC : 18f2680
-* Jan.21 2011
-*    ____________      _           _
-*   |___  /| ___ \    | |         | |
-*      / / | |_/ /___ | |__   ___ | |_
-*     / /  |    // _ \| '_ \ / _ \| __|
-*    / /   | |\ \ (_) | |_) | (_) | |_
-*   /_/    |_| \_\___/|____/ \___/'\__|
-*			      7robot.fr
-*/
+ * Programme PIC ARM petit robot
+ * Eurobot 2012
+ * Compiler : Microchip C18
+ * µC : 18f2680
+ * Jan.21 2011
+ *    ____________      _           _
+ *   |___  /| ___ \    | |         | |
+ *      / / | |_/ /___ | |__   ___ | |_
+ *     / /  |    // _ \| '_ \ / _ \| __|
+ *    / /   | |\ \ (_) | |_) | (_) | |_
+ *   /_/    |_| \_\___/|____/ \___/'\__|
+ *			      7robot.fr
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -53,12 +53,12 @@ void low_isr(void);
 
 
 /////*VARIABLES GLOBALES*/////
-int i=0;
+int i = 0;
 
 
 char rescp = 0; /*Compteur de réception USART. */
-char Rstate =0; /*Etat de réception*/
-char incoming=0; /*Byte entrant */
+char Rstate = 0; /*Etat de réception*/
+char incoming = 0; /*Byte entrant */
 
 
 
@@ -78,188 +78,173 @@ CANmsg * pmessage;
 /////*INTERRUPTIONS*/////
 
 #pragma code high_vector=0x08
-void high_interrupt(void)
-{
-     _asm GOTO high_isr _endasm
+
+void high_interrupt(void) {
+    _asm GOTO high_isr _endasm
 }
 #pragma code low_vector=0x18
-void low_interrupt(void)
-{
-     _asm GOTO low_isr _endasm
+
+void low_interrupt(void) {
+    _asm GOTO low_isr _endasm
 }
 #pragma code
 
 #pragma interrupt high_isr
-void high_isr(void)
-{
 
-    if(PIE1bits.RCIE && PIR1bits.RCIF)
-    {
+void high_isr(void) {
+
+    if (PIE1bits.RCIE && PIR1bits.RCIF) {
 
         // [ FD ] [ size | 0 | id10..8 ] [ id7..0] [ M1 ] [ M2 ] ? [ M8 ] [ BF ]
         incoming = ReadUSART();
-        if(incoming == 0xFD && Rstate == attenteFD)
-        {
+        if (incoming == 0xFD && Rstate == attenteFD) {
             Rstate = attenteSize;
-        }
-        else if(Rstate == attenteSize)
-        {
+        } else if (Rstate == attenteSize) {
             umessage.len = (incoming & 0xF0) >> 4; //TESTER 0 ET LEN
-            umessage.id =0;
-            ((char*)&umessage.id)[1] = incoming & 0b00000111;
+            umessage.id = 0;
+            ((char*) &umessage.id)[1] = incoming & 0b00000111;
             Rstate = attenteIdL;
-        }
-        else if(Rstate == attenteIdL)
-        {
-            *(char*)&umessage.id = incoming ;
-            if(umessage.len==0)
-            {
+        } else if (Rstate == attenteIdL) {
+            *(char*) &umessage.id = incoming;
+            if (umessage.len == 0) {
                 Rstate = verifBF;
-            }
-            else
-            {
+            } else {
                 Rstate = collecteData;
                 rescp = 0;
             }
-        }
-        else if(Rstate == collecteData)
-        {
-            umessage.data[rescp] = incoming ;
-            rescp ++;
-            if(rescp >=umessage.len)
-            {
-                Rstate = verifBF ;
+        } else if (Rstate == collecteData) {
+            umessage.data[rescp] = incoming;
+            rescp++;
+            if (rescp >= umessage.len) {
+                Rstate = verifBF;
             }
-        }
-        else if(Rstate == verifBF)
-        {
-            if(incoming == 0xBF)
-            {
+        } else if (Rstate == verifBF) {
+            if (incoming == 0xBF) {
                 pumessage = TrouverPlace(INbuffer);
                 *pumessage = umessage;
-                if(PlacesRestantes(INbuffer)==0)
+                if (PlacesRestantes(INbuffer) == 0)
                     printf("INbFULL!\n");
             }
-            Rstate=attenteFD;
+            Rstate = attenteFD;
         }
 
         PIR1bits.RCIF = 0;
     }
 
-        if(PIE3bits.RXB0IE && PIR3bits.RXB0IF)
-    {
+    if (PIE3bits.RXB0IE && PIR3bits.RXB0IF) {
 
         /*On stocke la valeur. */
-        while(CANIsRxReady())
-        {
-            CANReceiveMessage(&imessage.id,imessage.data,
-                          &imessage.len,&imessage.flags);
+        while (CANIsRxReady()) {
+            CANReceiveMessage(&imessage.id, imessage.data,
+                    &imessage.len, &imessage.flags);
         }
 
         /*On cherche une place dans la buffer... et on y place */
         pimessage = TrouverPlace(OUTbuffer);
         *pimessage = imessage;
 
-        if(imessage.id == 127)
-        {
+        if (imessage.id == 127) {
             _asm
                 reset
             _endasm
         }
 
-        if(PlacesRestantes(OUTbuffer)==0)
+        if (PlacesRestantes(OUTbuffer) == 0)
             printf("OUTbFULL!\n");
 
-        PIR3bits.RXB0IF=0;
-        PIR3bits.RXB1IF=0;
-        PIR3bits.ERRIF=0;
+        PIR3bits.RXB0IF = 0;
+        PIR3bits.RXB1IF = 0;
+        PIR3bits.ERRIF = 0;
 
     }
 
-    if(PIE3bits.ERRIE && PIR3bits.ERRIF)
-    {
+    if (PIE3bits.ERRIE && PIR3bits.ERRIF) {
         /*On stocke la valeur. */
-        while(CANIsRxReady())
-        {
-            CANReceiveMessage(&imessage.id,imessage.data,
-                          &imessage.len,&imessage.flags);
+        while (CANIsRxReady()) {
+            CANReceiveMessage(&imessage.id, imessage.data,
+                    &imessage.len, &imessage.flags);
+        }
+
+        if (imessage.id == 127) {
+            _asm
+                reset
+            _endasm
         }
 
 
         //On stocke le messgae mais on n'incremente pas le buffer
-        PIR3bits.RXB0IF=0;
-        PIR3bits.RXB1IF=0;
-        PIR3bits.ERRIF=0;
+        PIR3bits.RXB0IF = 0;
+        PIR3bits.RXB1IF = 0;
+        PIR3bits.ERRIF = 0;
     }
 
 
 }
 
 #pragma interrupt low_isr
-void low_isr(void)
-{
+
+void low_isr(void) {
 
 
 }
 
 
 /////*PROGRAMME PRINCIPAL*/////
-void main (void)
-{
+
+void main(void) {
     /* Initialisations. */
-    ADCON1 = 0x0F ;
+    ADCON1 = 0x0F;
     ADCON0 = 0b00000000;
-    WDTCON = 0 ;
+    WDTCON = 0;
 
     /* Configurations. */
-    TRISA   = 0b11111100 ;
-    TRISB   = 0b01111111 ;
-    TRISC   = 0b11111111 ;
+    TRISA = 0b11111100;
+    TRISB = 0b01111111;
+    TRISC = 0b11111111;
     on = 0;
     /*Configuration du port série*/
-    OpenUSART( USART_TX_INT_OFF & USART_RX_INT_ON
-                & USART_ASYNCH_MODE & USART_EIGHT_BIT
-                & USART_CONT_RX & USART_BRGH_HIGH, 10); //115200, 1,2% err...
+    OpenUSART(USART_TX_INT_OFF & USART_RX_INT_ON
+            & USART_ASYNCH_MODE & USART_EIGHT_BIT
+            & USART_CONT_RX & USART_BRGH_HIGH, 10); //115200, 1,2% err...
 
     /*Configuration du CAN*/
-    CANInitialize(1,5,7,6,2,CAN_CONFIG_VALID_STD_MSG);
+    CANInitialize(1, 5, 7, 6, 2, CAN_CONFIG_VALID_STD_MSG);
     Delay10KTCYx(200);
 
     // Interruptions Buffer1
-/*    IPR3bits.RXB1IP=1;// : priorité haute par defaut du buff 1
-    PIE3bits.RXB1IE=1;//autorise int sur buff1
-    PIR3bits.RXB1IF=0;//mise a 0 du flag*/
+    /*    IPR3bits.RXB1IP=1;// : priorité haute par defaut du buff 1
+        PIE3bits.RXB1IE=1;//autorise int sur buff1
+        PIR3bits.RXB1IF=0;//mise a 0 du flag*/
 
     // Interruption Buffer 0
-    IPR3bits.RXB0IP=1;// : priorité haute par defaut du buff 0
-    PIE3bits.RXB0IE=1;//autorise int sur buff0
-    PIR3bits.RXB0IF=0;//mise a 0 du flag
-    PIR3bits.ERRIF=0;
-    PIE3bits.ERRIE =1;
+    IPR3bits.RXB0IP = 1; // : priorité haute par defaut du buff 0
+    PIE3bits.RXB0IE = 1; //autorise int sur buff0
+    PIR3bits.RXB0IF = 0; //mise a 0 du flag
+    PIR3bits.ERRIF = 0;
+    PIE3bits.ERRIE = 1;
 
     // Configuration des masques et filtres
     // Set CAN module into configuration mode
     CANSetOperationMode(CAN_OP_MODE_CONFIG);
     // Set Buffer 1 Mask value
-    CANSetMask(CAN_MASK_B1, 0b0,CAN_CONFIG_STD_MSG);
+    CANSetMask(CAN_MASK_B1, 0b0, CAN_CONFIG_STD_MSG);
     // Set Buffer 2 Mask value
-    CANSetMask(CAN_MASK_B2, 0b0,CAN_CONFIG_STD_MSG );
+    CANSetMask(CAN_MASK_B2, 0b0, CAN_CONFIG_STD_MSG);
     // Set Buffer 1 Filter values
-    CANSetFilter(CAN_FILTER_B1_F1,0b0000,CAN_CONFIG_STD_MSG );
-    CANSetFilter(CAN_FILTER_B1_F2,0b0000,CAN_CONFIG_STD_MSG );
-    CANSetFilter(CAN_FILTER_B2_F1,0b0000,CAN_CONFIG_STD_MSG );
-    CANSetFilter(CAN_FILTER_B2_F2,0b0000,CAN_CONFIG_STD_MSG );
-    CANSetFilter(CAN_FILTER_B2_F3,0b0000,CAN_CONFIG_STD_MSG );
-    CANSetFilter(CAN_FILTER_B2_F4,0b0000,CAN_CONFIG_STD_MSG );
+    CANSetFilter(CAN_FILTER_B1_F1, 0b0000, CAN_CONFIG_STD_MSG);
+    CANSetFilter(CAN_FILTER_B1_F2, 0b0000, CAN_CONFIG_STD_MSG);
+    CANSetFilter(CAN_FILTER_B2_F1, 0b0000, CAN_CONFIG_STD_MSG);
+    CANSetFilter(CAN_FILTER_B2_F2, 0b0000, CAN_CONFIG_STD_MSG);
+    CANSetFilter(CAN_FILTER_B2_F3, 0b0000, CAN_CONFIG_STD_MSG);
+    CANSetFilter(CAN_FILTER_B2_F4, 0b0000, CAN_CONFIG_STD_MSG);
     // Set CAN module into Normal mode
     CANSetOperationMode(CAN_OP_MODE_NORMAL);
 
 
     /* Signal de démarrage du programme. */
     led = 0;
-    for(i=0;i<20;i++)
-    {
-        led=led^1;
+    for (i = 0; i < 20; i++) {
+        led = led^1;
         DelayMS(50);
     }
     led = 0;
@@ -275,28 +260,25 @@ void main (void)
 
 
     /* Boucle principale. */
-     while(1)
-    {
-         /**/
-         if(PlacesRestantes(INbuffer) < 4)
-         {
-             pmessage = TrouverMessage(INbuffer);
-             message = *pmessage;
-             led = led^1;
-             CANSendMessage(message.id,message.data,
-                        message.len,CAN_TX_PRIORITY_0 & CAN_TX_STD_FRAME & CAN_TX_NO_RTR_FRAME );
-             
-             pmessage->len = 10;
-         }
+    while (1) {
+        /**/
+        if (PlacesRestantes(INbuffer) < 4) {
+            pmessage = TrouverMessage(INbuffer);
+            message = *pmessage;
+            led = led^1;
+            CANSendMessage(message.id, message.data,
+                    message.len, CAN_TX_PRIORITY_0 & CAN_TX_STD_FRAME & CAN_TX_NO_RTR_FRAME);
 
-         /* On envoie 1 message du buffer USART*/
-         if(PlacesRestantes(OUTbuffer) < 4)
-         {
-             pmessage = TrouverMessage(OUTbuffer);
-             led = led^1;
-             CANtoUSART(pmessage);
-             pmessage->len = 10;
-         }
+            pmessage->len = 10;
+        }
+
+        /* On envoie 1 message du buffer USART*/
+        if (PlacesRestantes(OUTbuffer) < 4) {
+            pmessage = TrouverMessage(OUTbuffer);
+            led = led^1;
+            CANtoUSART(pmessage);
+            pmessage->len = 10;
+        }
     }
 }
 
