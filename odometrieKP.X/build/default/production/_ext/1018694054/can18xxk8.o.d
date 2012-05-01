fa459b2c07f99738b21ba9b7429588154da9e660
Maxime Morin <maxime.morin7@gmail.com>
2012-05-01 15:50:57 +0200

    Force

---
+++ odometrieKP.X/build/default/production/_ext/1018694054/can18xxk8.o.d
@@ -0,0 +1,5 @@
+build/default/production/_ext/1018694054/can18xxk8.o : \
+	/home/max/7robot/Eurobot2012/PIC/libcan/can18xxk8.c \
+	/home/max/7robot/Eurobot2012/PIC/libcan/can18xx8.h \
+	/opt/microchip/mplabc18/v3.40/h/p18f25k80.h \
+
---
+++ odometrieKP.X/build/default/production/odometrieKP.o.d
@@ -0,0 +1,15 @@
+build/default/production/odometrieKP.o : \
+	/home/max/7robot/Eurobot2012/PIC/odometrieKP.X/odometrieKP.c \
+	/opt/microchip/mplabc18/v3.40/h/stdio.h \
+	/opt/microchip/mplabc18/v3.40/h/stdlib.h \
+	/opt/microchip/mplabc18/v3.40/h/delays.h \
+	/opt/microchip/mplabc18/v3.40/h/timers.h \
+	/opt/microchip/mplabc18/v3.40/h/math.h \
+	/opt/microchip/mplabc18/v3.40/h/portb.h \
+	/home/max/7robot/Eurobot2012/PIC/libcan/can18xxk8.h \
+	/opt/microchip/mplabc18/v3.40/h/stdarg.h \
+	/opt/microchip/mplabc18/v3.40/h/stddef.h \
+	/opt/microchip/mplabc18/v3.40/h/p18f25k80.h \
+	/opt/microchip/mplabc18/v3.40/h/p18cxxx.h \
+	/opt/microchip/mplabc18/v3.40/h/pconfig.h \
+
